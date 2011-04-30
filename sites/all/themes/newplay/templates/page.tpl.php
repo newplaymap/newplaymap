4136a7c9d1966062729596bf245a3c680e205550
Colin Sagan <colin@quilted.coop>
2011-04-29 17:25:12 -0700

    Try just the Open Graph title.

--- sites/all/themes/newplay/templates/page.tpl.php
+++ sites/all/themes/newplay/templates/page.tpl.php
@@ -112,6 +112,7 @@ $domain = (isset($_SERVER['HTTP_HOST'])) ? 'http://' . preg_replace('`^www.`', '
 <head>
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
+  <meta property="og:title" content="New Play Map" />
   <meta property="og:url" content="http://newplaymap.org" />
   <meta property="fb:admins" content="100000508562653" />
   <meta property="og:type" content="activity" />
